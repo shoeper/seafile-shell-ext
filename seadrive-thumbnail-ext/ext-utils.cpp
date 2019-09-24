e0d60aaf1f67a2062fc703b9baf3015d87b46e87
Shuai Lin <linshuai2012@gmail.com>
2019-09-24 19:43:40 +0800

    Fixed a crash bug

--- seadrive-thumbnail-ext/ext-utils.cpp
+++ seadrive-thumbnail-ext/ext-utils.cpp
@@ -329,9 +329,6 @@ std::string splitPath(const std::string& path, int* pos)
     }
 
     std::string p = normalizedPath(path);
-    while (p.size() > 1 && p[-1] == '/') {
-        p = p.substr(0, p.size() - 1);
-    }
     if (p.size() == 1) {
         return p;
     }
@@ -363,9 +360,6 @@ std::string getDiskLetterPath(const std::string& path, int* pos)
     }
 
     std::string p = normalizedPath(path);
-    while (p.size() > 1 && p[-1] == '/') {
-        p = p.substr(0, p.size() - 1);
-    }
     if (p.size() == 1) {
         return p;
     }
